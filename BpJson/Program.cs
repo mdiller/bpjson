0c5f32c41a71979cffa35a512ce13829cf417de4
Malcolm Diller <malcolm@dillerm.io>
2019-09-05 18:05:00 -0700

    fixed indentation to use tabs

--- BpJson/Program.cs
+++ BpJson/Program.cs
@@ -2,11 +2,11 @@
 
 namespace BpJson
 {
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      Console.WriteLine("Hello World!");
-    }
-  }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
 }
--- BpJson/StructureEnums.cs
+++ BpJson/StructureEnums.cs
@@ -4,44 +4,44 @@ using System.Text;
 
 namespace BpJson
 {
-    /// <summary>
-    /// The different kinds of items that can be stored in json
-    /// </summary>
-    public enum JsonItem
-    {
-        /// <summary>
-        /// Marks the end of an Object or Array
-        /// </summary>
-        EndOfItem,
+	/// <summary>
+	/// The different kinds of items that can be stored in json
+	/// </summary>
+	public enum JsonItem
+	{
+		/// <summary>
+		/// Marks the end of an Object or Array
+		/// </summary>
+		EndOfItem,
 
-        /// <summary>
-        /// Marks the beginning of a json object
-        /// </summary>
-        Object,
+		/// <summary>
+		/// Marks the beginning of a json object
+		/// </summary>
+		Object,
 
-        /// <summary>
-        /// Marks the beginning of a json array
-        /// </summary>
-        Array,
+		/// <summary>
+		/// Marks the beginning of a json array
+		/// </summary>
+		Array,
 
-        /// <summary>
-        /// Marks the beginning of a string
-        /// </summary>
-        String,
+		/// <summary>
+		/// Marks the beginning of a string
+		/// </summary>
+		String,
 
-        /// <summary>
-        /// Marks the beginning of a number
-        /// </summary>
-        Number,
+		/// <summary>
+		/// Marks the beginning of a number
+		/// </summary>
+		Number,
 
-        /// <summary>
-        /// Marks the beginning of a boolean
-        /// </summary>
-        Boolean,
+		/// <summary>
+		/// Marks the beginning of a boolean
+		/// </summary>
+		Boolean,
 
-        /// <summary>
-        /// Marks a null
-        /// </summary>
-        Null
-    }
+		/// <summary>
+		/// Marks a null
+		/// </summary>
+		Null
+	}
 }
